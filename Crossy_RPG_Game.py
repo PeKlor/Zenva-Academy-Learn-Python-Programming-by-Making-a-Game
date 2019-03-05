b5078aa0d2c44e58ebcaf5629719d42054a6aeda
PeKlor <peklor7@gmail.com>
2019-03-05 23:22:48 +0200

    implementing Player class and basic movement

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -27,6 +27,10 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+
+        player_character = PlayerCharacter('player.png', 375, 700, 50, 50)
+
         # main game loop, used to update all gameplay such as movement, checks, and graphics
         while not is_game_over:
 
@@ -36,8 +40,27 @@ class Game:
                 # if we have a quit type event (exit out) then exit out of the game loop
                 if event.type == pygame.QUIT:
                     is_game_over = True
-
+                # detect when key is pressed down
+                elif event.type == pygame.KEYDOWN:
+                    # move up if the up key is pressed
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    # move down if the down key is pressed
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                # detect when key is released
+                elif event.type == pygame.KEYUP:
+                    # stop movement when key no longer pressed
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
                 print(event)
+
+            # redraw the screen to be a blank white window
+            self.game_screen.fill(WHITE_COLOR)
+            # update the player position
+            player_character.move(direction)
+            # draw the player at the new position
+            player_character.draw(self.game_screen)
 
             # update all game graphics
             pygame.display.update()
@@ -56,9 +79,29 @@ class GameObject:
         self.x_pos = x
         self.y_pos = y
 
+        self.width = width
+        self.height = height
+
     def draw(self, background):
         # "blit" means draw; we draw the background with the image, the left corner of it being (x, y)
         background.blit(self.image, (self.x_pos, self.y_pos))
+
+
+# the class to represent the character controlled by the player
+class PlayerCharacter(GameObject):
+
+    # how many tiles the character moves per second
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # move function will move character up if direction > 0 and down if < 0
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 
 pygame.init()
@@ -66,11 +109,14 @@ pygame.init()
 new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
 new_game.run_game_loop()
 
+# Load the player image from the file directory
+# player_image = pygame.image.load('player.png')
+# Scale the image up
+# player_image = pygame.transform.scale(player_image, (50, 50))
 
 # quit pygame and the program
 pygame.quit()
 quit()
-
 
 # draw a rectangle on top of the game screen canvas (x, y, width, height)
 # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
